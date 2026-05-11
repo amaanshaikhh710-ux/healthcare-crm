596c1c8c31f3de9108566af1a66225770467f83c
amaanshaikhh710-ux <amaanshaikhh710@gmail.com>
2026-05-11 14:59:18 +0530

    Update DATABASE_MIGRATION.sql

--- healthcare-crm/DATABASE_MIGRATION.sql
+++ healthcare-crm/DATABASE_MIGRATION.sql
@@ -126,6 +126,7 @@ CREATE TABLE IF NOT EXISTS campaign_logs (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS users (
     doctor_id SERIAL PRIMARY KEY,
+    name varchar(255),
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
